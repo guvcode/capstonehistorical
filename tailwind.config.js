b7a5923cfce90d06dab1cbcfe2387ac1ad4bd86d
guvcode <gbengaomoni@gmail.com>
2023-09-25 23:01:41 -0600

    still on the white background

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
       current: "currentColor",
       transparent: "transparent",
       white: "#FFFFFF",
-      black: "#FFFFFF",
+      black: "#090E34",
       dark: "#1D2144",
       primary: "#4A6CF7",
       yellow: "#FBB040",
@@ -24,7 +24,6 @@ module.exports = {
     screens: {
       xs: "450px",
       // => @media (min-width: 450px) { ... }
-
       sm: "575px",
       // => @media (min-width: 576px) { ... }
 
